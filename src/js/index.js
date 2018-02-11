3e6f05d40da6cc8ee930fc18d9527815efc10098
Andrushkins <drachyov.andrew@gmail.com>
2018-02-11 15:22:00 +0300

    project switcher update

--- src/js/index.js
+++ src/js/index.js
@@ -5,26 +5,30 @@ import '../styles/main.scss';
 document.addEventListener('DOMContentLoaded', () => {
   const projects = document.querySelectorAll('.project');
 
-  const prevBtn = document.querySelector('#prev-btn');
-  const nextBtn = document.querySelector('#next-btn');
+  const prevBtn = document.querySelectorAll('.prev-btn');
+  const nextBtn = document.querySelectorAll('.next-btn');
 
   let current = 0;
 
-  prevBtn.addEventListener('click', () => {
-    projects[current].classList.remove('active');
-    current-=1;
-    if (current<0) {
-      current = projects.length - 1;
-    }
-    projects[current].classList.add('active');
+  prevBtn.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      projects[current].classList.remove('active');
+      current-=1;
+      if (current<0) {
+        current = projects.length - 1;
+      }
+      projects[current].classList.add('active');
+    });
   });
 
-  nextBtn.addEventListener('click', () => {
-    projects[current].classList.remove('active');
-    current+=1;
-    if (current>projects.length - 1) {
-      current = 0;
-    }
-    projects[current].classList.add('active');
+  nextBtn.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      projects[current].classList.remove('active');
+      current+=1;
+      if (current>projects.length - 1) {
+        current = 0;
+      }
+      projects[current].classList.add('active');
+    });
   });
 });
