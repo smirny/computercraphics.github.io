d929f2d8b85ad1a16cdf57e94bba250a7c3341fb
Andrushkins <drachyov.andrew@gmail.com>
2018-02-11 11:46:13 +0300

    poster project

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,28 @@
 import '../styles/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('asd from main asd');
+  const projects = document.querySelectorAll('.project');
+
+  const prevBtn = document.querySelector('#prev-btn');
+  const nextBtn = document.querySelector('#next-btn');
+
+  let current = 0;
+
+  prevBtn.addEventListener('click', () => {
+    projects[current].classList.remove('active');
+    current-=1;
+    if (current<0) {
+      current = projects.length - 1;
+    }
+    projects[current].classList.add('active');
+  });
+
+  nextBtn.addEventListener('click', () => {
+    projects[current].classList.remove('active');
+    current+=1;
+    if (current>projects.length - 1) {
+      current = 0;
+    }
+    projects[current].classList.add('active');
+  });
 });
